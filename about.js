1a1dec4ea6f00153c99912e63085fdc15b610255
iamlove <love231992@gmail.com>
2020-04-16 16:31:49 +0530

    ready

---
+++ about.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Text, StyleSheet, View, ImageBackground, Image,Linking,TouchableOpacity,StatusBar } from 'react-native';
+import { MaterialIcons, AntDesign } from "@expo/vector-icons";
+
+export default function About({navigation}) {
+
+    const openMenu = () => {
+        navigation.openDrawer()
+    }
+    return (
+        <ImageBackground source={require("../NPL_Ash/assets/bg.jpg")} style = {styles.bgconatiner}>
+           <MaterialIcons name="menu" size={40} style={styles.icon} onPress={openMenu} />
+           
+        <Image source={require("../NPL_Ash/assets/logo1.jpg")} style = {styles.logo} />
+        <Text style = {styles.logotext}>This app is for departmental use only. To keep the privacy of the data, request you to please don't share it.</Text>
+        
+        <TouchableOpacity onPress={() => Linking.openURL("https://github.com/love231992/Node-express-API")}>
+            <View style = {styles.link}>
+            <AntDesign name="github" size={20} color="white" style = {styles.giticon}/>
+                <Text style={styles.linkText}>Open Sourced on GITHUB</Text>
+            </View>
+        </TouchableOpacity>
+        <Text style = {styles.footer}>Crafted by Lovepreet Singh</Text>
+     
+        </ImageBackground>
+    )
+        
+}
+
+
+const styles = StyleSheet.create({
+    bgconatiner:{
+        flex:1,
+        width: null,
+        height: null,
+        justifyContent:"space-between",
+        alignItems:"center",
+
+    },
+    
+    logo:{
+        width:120,
+        height:120,
+        opacity:0.8,
+        marginTop:40
+    },
+    logoconatiner:{
+        alignItems:"center"
+    },
+    logotext:{
+        color:"#d108b6",
+        fontSize:18,
+        fontWeight:"bold",
+        marginTop:150,
+        marginHorizontal:20,
+        textAlign:"center"
+    },
+    icon:{
+        position:"absolute",
+        right:5,
+        color:"crimson",
+        marginTop:100   
+      },
+      footer:{
+          justifyContent:"flex-end",
+          marginBottom:10,
+          fontSize:15,
+          fontWeight:"bold",
+          color:"crimson",
+          opacity:0.8,
+          letterSpacing:2.5
+      },
+      link:{
+          flexDirection:"row",
+          justifyContent:"center",
+          backgroundColor:"black",
+          borderRadius:5,
+          marginTop:180,
+          opacity:0.9
+      },
+      linkText:{
+          color:"white",padding:10,
+
+      },giticon:{
+          marginTop:7,
+          marginLeft:4
+      }
+      
+      
+})
---
+++ home.js
@@ -0,0 +1,147 @@
+import React, { useState } from 'react'
+import { Text, StyleSheet, View, Alert, FlatList, ImageBackground, TouchableOpacity, ActivityIndicator,StatusBar } from 'react-native'
+import { MaterialIcons } from "@expo/vector-icons";
+
+
+export default function App({navigation}) {
+  const [doList,setDoList] = useState();
+  var [loading,setLoading] = useState(true)
+
+  connect = () => {
+      setLoading(true)
+      const URL = "https://npl-ash.herokuapp.com/";
+      fetch(URL).then(response => response.json())
+      .then(response => {
+          setDoList(response)
+      }).catch(error => {
+          Alert.alert(error.message);
+      }
+      ).then(setTimeout(() =>{
+        setLoading(false)
+      },2000)
+      )
+  }
+
+  const openMenu = () => {
+    navigation.openDrawer()
+}
+  return (
+      <View style = {styles.component}>
+        <StatusBar hidden={true} />
+          <ImageBackground source={require("../NPL_Ash/assets/new.png")} style = {styles.header}>
+            <MaterialIcons name="menu" size={40} style={styles.icon} onPress={openMenu} />
+              <View style={styles.headerTitle}>
+                <Text style={styles.headerText}>NPL ASH MANAGEMENT</Text>
+              </View>
+          </ImageBackground>
+          <TouchableOpacity onPress={connect}>
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>Connect to the server</Text>
+            </View>   
+          </TouchableOpacity>
+          {loading ?
+          (
+            <View style={styles.indicator}>
+            <ActivityIndicator size="large" color="crimson" />
+            </View>
+          ):
+
+          (
+          <FlatList
+            data={doList}
+            keyExtractor={(item) => item[1] }
+            renderItem={({item }) => (
+            <TouchableOpacity activeOpacity={0.4} >
+              <View style={styles.item}>  
+                <Text style={styles.itemText}>{item [0]}</Text>
+                <Text style={styles.itemText}>{item [1]}</Text>
+              </View>
+              </TouchableOpacity>
+            )}
+            />
+          )}
+          <Text style = {styles.footer}>Crafted by Lovepreet Singh</Text>
+      </View>  
+  )
+}
+
+
+const styles = StyleSheet.create({
+  component:{
+      backgroundColor:"white",
+      flex:1,
+  },
+  header:{
+    flexDirection:"row",
+    alignItems:"flex-end",
+    justifyContent:"center",
+    height:120,
+    marginBottom:0
+  },
+  icon:{
+    position:"absolute",
+    right:5,
+    color:"white",
+
+  },
+  headerTitle:{
+    flexDirection:"row",
+  },
+  headerText:{
+    fontWeight:"bold",
+    fontSize:20,
+    color:"white",
+    letterSpacing:2.3,
+    marginTop:15,
+    height:"100%",
+    alignItems:"center"
+  },
+  itemText:{
+      marginLeft:10,
+      marginRight:10,
+      fontSize: 20,
+      fontWeight:"bold",
+      color:"crimson",
+  },
+  item:{
+      flex:1,
+      flexDirection:"row",
+      justifyContent:"space-between",
+      backgroundColor: "#E6FDFF",
+      margin: 2,
+      padding:15,
+      fontSize:20,
+      borderRadius:2,
+      borderColor:"black",
+      borderWidth:0.2,
+      borderStyle:"solid",
+      elevation:5,
+      opacity:0.8
+  },
+  button:{
+    paddingVertical:14,
+    paddingHorizontal:10,
+    backgroundColor:"#f01d71"
+},
+buttonText:{
+    color:"white",
+    fontWeight:"bold",
+    fontSize:16,
+    textAlign:"center",
+    letterSpacing:2
+},
+indicator:{
+  flex:1,
+  justifyContent:"center"
+},
+footer:{
+  textAlign:"center",
+  padding:5,
+  fontSize:15,
+  fontWeight:"bold",
+  color:"crimson",
+  opacity:0.8,
+  letterSpacing:2.5,
+  backgroundColor:"#DAB785"
+},
+})
